a56ddf626617b4d895b9cf19a417481adcecb88e
D41TZ <lucascarnevallicunha.lcc@gmail.com>
2020-09-16 09:38:55 -0300

    .

---
+++ Assets/mouseLook.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+    public Transform PlayerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+
+        PlayerBody.Rotate(Vector3.up * mouseX);
+    }
+}
