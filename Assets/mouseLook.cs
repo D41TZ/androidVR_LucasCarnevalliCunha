08d7385e6d2de80c56275c7e98ff0231fff6b5d4
D41TZ <lucascarnevallicunha.lcc@gmail.com>
2020-10-02 11:05:38 -0300

    ultima_aula

--- Assets/mouseLook.cs
+++ Assets/mouseLook.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class mouseLook : MonoBehaviour
 {
+    public float mouseSensitivy = 100f;
     public Transform PlayerBody;
+    float xRotation = 0f; 
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X");
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivy * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivy * Time.deltaTime;
 
         PlayerBody.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
     }
 }
